7dfa279bdb706f0a1e37ee9628937f2998feec96
aribakha <140427904+aribakha@users.noreply.github.com>
2023-12-08 15:53:13 +0500

    pdf assignment

---
+++ app.js
@@ -0,0 +1,65 @@
+                                    //  CHAPTER # 26-30
+
+                                      // Quesstion # 1
+
+// var number = "3.45214";
+// var roundOff = Math.round(number);
+// var floor = Math.floor(number);
+// var  ceil = Math.ceil(number);
+// document.write("Number : " + number + "</br>")
+// document.write("Round off value : " + roundOff + "</br>")                                  
+// document.write("Floor value : " + floor + "</br>")                                  
+// document.write("Ceil value : " + ceil )               
+
+    //  Example for practice :
+// var originalNumber = 7.85;
+// var ceilValue = Math.ceil(originalNumber);
+
+// document.write("Original Number:", originalNumber + "</br>");
+// document.write("Ceil Value:", ceilValue);
+
+                                      // Quesstion # 2
+
+// var originalNumber = "-2.673"; 
+// var roundOff = Math.round(originalNumber);
+// var floor = Math.floor(originalNumber);
+// var ceil = Math.ceil(originalNumber);
+// document.write("Original number : " + originalNumber + "</br>");
+// document.write("Round off value : " + roundOff + "</br>");
+// document.write("Original number : " + floor + "</br>");
+// document.write("Original number : " + ceil )
+
+                                      // Quesstion # 4
+                        
+// var diceValue = Math.floor(Math.random()*6) + 1; 
+// document.write("Random dice value : " + diceValue)  
+
+// var diceValue = Math.floor(Math.random()*4) + 1;
+// document.write("Random dice value : " + diceValue)
+
+                                      // Quesstion # 5
+
+
+  // let randomNumber = getRandomNumber(1, 100);
+  // function getRandomNumber(min, max) {
+  //   return Math.floor(Math.random() * (max - min + 1)) + min;
+  // }
+  // document.write("Random number between 1 and 100 :" + randomNumber);
+
+                                      // Quesstion # 6
+
+// function user(){
+//   var userInput = prompt("Enter your weight (e.g 50 ,50kgs,50.2kgs,0.2 kilogram)");
+//   var weight  = parseFloat(userInput);
+
+//   document.write("Your weight is " + weight + " kilograms")
+// }  
+// user();                                    
+
+                                      // Quesstion # 7
+
+// var randomNumber = getRandomNumber(1,10);
+// function getRandomNumber(min , max){
+//   return Math.floor(Math.random()* (max - min) + min );
+// }
+// document.write("Randon number between 1 to 10 is : " + randomNumber);                                     
